c75db62c753d221d5b5a442fbef9ea8881f1236f
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-26 20:51:39 -0500

    added Menu component for the menu drawer

--- web/src/components/menu-bar/MenuBar.tsx
+++ web/src/components/menu-bar/MenuBar.tsx
@@ -1,15 +1,20 @@
 import Box from "@mui/material/Box";
 import {AppBar, IconButton, Toolbar} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from "./Menu.tsx";
+import {useOpenable} from "../../hooks/useOpenable.tsx";
 
 export default function MenuBar() {
+    const { isOpen: isMenuOpen, setIsOpen: setIsMenuOpen, toggleOpen: toggleMenuOpen } = useOpenable();
+
     return (
-        <Box className={'flex-grow'}>
+        <Box>
             <AppBar position={'static'}>
                 <Toolbar>
-                    <IconButton size={'large'} aria-label={'Menu'}>
+                    <IconButton size={'large'} aria-label={'Menu'} onClick={toggleMenuOpen}>
                         <MenuIcon />
                     </IconButton>
+                    <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
                 </Toolbar>
             </AppBar>
         </Box>
