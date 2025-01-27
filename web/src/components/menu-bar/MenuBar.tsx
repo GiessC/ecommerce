8d9033d954752ae58f49adfc5a3c86d5be99c22b
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-01-26 20:40:51 -0500

    added hamburger menu button to MenuBar

--- web/src/components/menu-bar/MenuBar.tsx
+++ web/src/components/menu-bar/MenuBar.tsx
@@ -1,11 +1,15 @@
 import Box from "@mui/material/Box";
-import {AppBar, Toolbar} from "@mui/material";
+import {AppBar, IconButton, Toolbar} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function MenuBar() {
     return (
         <Box className={'flex-grow'}>
             <AppBar position={'static'}>
                 <Toolbar>
+                    <IconButton size={'large'} aria-label={'Menu'}>
+                        <MenuIcon />
+                    </IconButton>
                 </Toolbar>
             </AppBar>
         </Box>
